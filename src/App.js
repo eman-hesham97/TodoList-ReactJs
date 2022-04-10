71ceee2acd2c4a447f23e571c81b6842c040500a
eman-hesham97 <eng.eman.eltaweel@gmail.com>
2022-04-10 23:49:45 +0200

    edit task

--- src/App.js
+++ src/App.js
@@ -1,121 +1,170 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import React from "react";
-import './App.css';
+import "./App.css";
 import background from "./5.jpg";
 
-function TodoInput ({ onAdd }){
-  let [value, setValue] = useState("");
+function TodoInput({ onSubmit, label = "Add Todo", inputValue }) {
+  let [value, setValue] = useState(inputValue);
   return (
-    <div className='ipField'>
-      <input className='ipTxt'
-      type="text"
-      onChange={(e)=>{
-        value = e.target.value;
-      }
-      }>
-      </input>
+    <div className="ipField">
+      <input
+        className="ipTxt"
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></input>
       <br></br>
-      <button className='ipBtn'
-        onClick={()=>{
-          onAdd(value);
+      <button
+        className="ipBtn"
+        onClick={() => {
+          onSubmit(value);
           setValue("");
-        }}>
-        Add Todo
+        }}
+      >
+        {label}
       </button>
     </div>
-  )
+  );
 }
 
-function TodoList({items, onDelete,completeTodo, header,onChecked}){
-  return(
+function TodoList({
+  items,
+  onDelete,
+  completeTodo,
+  header,
+  onChecked,
+  onUpdate,
+}) {
+  const [inEdit, setInEdit] = useState(null);
+  return (
     <>
-    {header}
-    <ul className='mylist'>
-      {items.map((item)=>(
-        <TodoItem
-        key={item.id}
-        value = {item}
-        onDelete={()=>{
-          onDelete(item);
-        }}
-        onChecked={()=>{
-          onChecked(item);
-        }}
-        completeTodo={()=>{
-          completeTodo(item);
-        }}
-        >
-        {(title) => <span>{title}</span>}
-        </TodoItem>
-      ))}
-    </ul>
+      {header}
+      <ul className="mylist">
+        {items.map((item) =>
+          inEdit === item.id ? (
+            <TodoInput
+              inputValue={item.title}
+              label="Save"
+              onSubmit={(value) => {
+                onUpdate(item, value);
+                setInEdit(null);
+              }}
+            />
+          ) : (
+            <TodoItem
+              key={item.id}
+              value={item}
+              onDelete={() => {
+                onDelete(item);
+              }}
+              onChecked={() => {
+                onChecked(item);
+              }}
+              onDoubleClick={(value) => setInEdit(value)}
+              // completeTodo={()=>{
+              //   completeTodo(item);
+              // }}
+            >
+              {(title) => <span>{title}</span>}
+            </TodoItem>
+          )
+        )}
+      </ul>
     </>
   );
 }
 
-function TodoItem({value, onDelete, index, completeTodo, children,onChecked}){
-  return(
+function TodoItem({
+  value,
+  onDelete,
+  index,
+  completeTodo,
+  children,
+  onChecked,
+  onDoubleClick,
+}) {
+  return (
     <div>
-    <li className='list-group-item'>
-      <input type="checkbox" checked={value.isCompleted} onChange={() =>{
-        onChecked(value);
-      }} />
-      {value.title}
-      
-      <button className='xBtn' onClick={()=> onDelete()}>X</button>
-    </li>
-    <br></br>
-    </div>
+      <br></br>
+      <li
+        className="list-group-item"
+        onDoubleClick={() => onDoubleClick(value.id)}
+      >
+        <input
+          type="checkbox"
+          checked={value.isCompleted}
+          onChange={() => {
+            onChecked(value);
+          }}
+        />
+        {value.title}
 
+        <button className="xBtn" onClick={() => onDelete()}>
+          X
+        </button>
+      </li>
+    </div>
   );
 }
 
-
-function App(){
+function App() {
   const [items, setItems] = useState([
-    {id:1, title:"item1", isCompleted:false},
-    {id:2, title:"item2", isCompleted:false},
-    {id:3, title:"item3", isCompleted:false},
+    { id: 1, title: "item1", isCompleted: false },
+    { id: 2, title: "item2", isCompleted: false },
+    { id: 3, title: "item3", isCompleted: false },
   ]);
-
-  const completeTodo = index => {
-    const newTodos = [...items];
-    newTodos[index].isCompleted = true;
-    setItems(newTodos);
-  };
+  
+    const handleUpdate = (item, value) =>{
+      setItems(
+        items.map((TodoItem) =>
+          TodoItem.id === item.id
+            ? { ...TodoItem, title: value }
+            : TodoItem
+        )
+      );
+    }
+  // const completeTodo = index => {
+  //   const newTodos = [...items];
+  //   newTodos[index].isCompleted = true;
+  //   setItems(newTodos);
+  // };
 
   // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/todos')
-  // .then(response => response.json())
-  // .then((json) => setItems(json));
+  //   fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
   // }, []);
-  
-  return(
-    <div style={{backgroundImage: `url(${background})`}}>
+
+  return (
+    <div style={{ backgroundImage: `url(${background})` }}>
       <TodoInput
-      onAdd={(value)=>{
-        setItems([
-          ...items,
-          {id:Math.random(), title:value, isCompleted:false},
-        ]);
-      }}
+        onSubmit={(value) => {
+          setItems([
+            ...items,
+            { id: Math.random(), title: value, isCompleted: false },
+          ]);
+        }}
       />
-      <TodoList 
-      header={<h2 className='title'>
-        ToDo List Items:
-      </h2>}
-      items={items} 
-      onDelete={(item)=>{
-        setItems(items.filter((TodoItem)=>TodoItem.id !== item.id));
-      }}
-      // completeTodo={completeTodo}
-      onChecked={(item)=>{
-        setItems(items.map((TodoItem)=>TodoItem.id === item.id 
-        ? {...TodoItem, isCompleted:!TodoItem.isCompleted}
-        :TodoItem));
-      }}
+      <TodoList
+        header={<h2 className="title">ToDo List Items:</h2>}
+        items={items}
+        onDelete={(item) => {
+          setItems(items.filter((TodoItem) => TodoItem.id !== item.id));
+        }}
+        // completeTodo={completeTodo}
+        onChecked={(item) => {
+          setItems(
+            items.map((TodoItem) =>
+              TodoItem.id === item.id
+                ? { ...TodoItem, isCompleted: !TodoItem.isCompleted }
+                : TodoItem
+            )
+          );
+        }}
+        onUpdate={handleUpdate}
       />
-      
     </div>
   );
 }
